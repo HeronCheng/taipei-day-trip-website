b57cdd29ddc147664d3dcfcf2995764623ee293b
Heron Cheng <u101003418@cmu.edu.tw>
2022-03-03 10:23:08 +0800

    revise account

--- cnxpool.py
+++ cnxpool.py
@@ -7,5 +7,5 @@ cnxpool=mysql.connector.pooling.MySQLConnectionPool(pool_name = "mypool",
                                                         host='localhost',
                                                         port='3306',
                                                         user='root',
-                                                        password='134679258',
+                                                        password='123456',
                                                         database='website')
