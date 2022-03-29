e4497fef09e21498f79a2a9e2b62c2edf9df2dc7
Heron Cheng <u101003418@cmu.edu.tw>
2022-03-30 00:27:23 +0800

    fix the problem that username can not  be showed on the website

--- static/booking.js
+++ static/booking.js
@@ -24,53 +24,56 @@ function checkStatus(){
         if(statusData.data==null){
             window.location.href = "/";
         }
-        username=statusData.data.name;
-        useremail=statusData.data.email;
-    }
-    //抓取cookie中的資料
-    let bookreq=new XMLHttpRequest();
-    bookreq.open("get","/api/booking");
-    bookreq.withCredentials = true;
-    bookreq.send();
-    bookreq.onload=function(){
-        user_name=document.getElementById("username");
-        user_name.innerHTML=username;
-        bookData=JSON.parse(bookreq.responseText);
-        if(bookData.data==null){
-            first.innerHTML="";
-            first_part.style.height="220px";
-            line1.style.display="none";
-            line2.style.display="none";
-            line3.style.display="none";
-            second.innerHTML="";
-            second.style.display="none";
-            third.innerHTML="";
-            third.style.display="none";
-            forth.innerHTML="";
-            forth.style.display="none";
-            nodatahere.style.display="block";
-            nodatahere.innerHTML="目前沒有任何待預訂的行程";
-            footer.style.height="865px";
-        }
         else{
-            let pic=document.getElementById("picture");
-            pic.src=bookData.data.attraction.image;
-            let b_title=document.getElementById("b_title");
-            b_title.innerHTML=bookData.data.attraction.name;
-            let b_date=document.getElementById("b_date");
-            b_date.innerHTML=bookData.data.date;
-            let b_time=document.getElementById("b_time");
-            b_time.innerHTML=bookData.data.time;
-            let b_fee=document.getElementById("b_fee");
-            let total_fee=document.getElementById("total_fee");
-            b_fee.innerHTML=bookData.data.price;
-            total_fee.innerHTML=bookData.data.price;
-            let b_address=document.getElementById("b_address");
-            b_address.innerHTML=bookData.data.attraction.address;
-            let contactperson=document.getElementById("contactperson");
-            contactperson.value=username;
-            let contact_email=document.getElementById("contact_email");
-            contact_email.value=useremail;
+            username=statusData.data.name;
+            useremail=statusData.data.email;
+
+            //抓取cookie中的資料
+            let bookreq=new XMLHttpRequest();
+            bookreq.open("get","/api/booking");
+            bookreq.withCredentials = true;
+            bookreq.send();
+            bookreq.onload=function(){
+                user_name=document.getElementById("username");
+                user_name.innerHTML=username;
+                bookData=JSON.parse(bookreq.responseText);
+                if(bookData.data==null){
+                    first.innerHTML="";
+                    first_part.style.height="220px";
+                    line1.style.display="none";
+                    line2.style.display="none";
+                    line3.style.display="none";
+                    second.innerHTML="";
+                    second.style.display="none";
+                    third.innerHTML="";
+                    third.style.display="none";
+                    forth.innerHTML="";
+                    forth.style.display="none";
+                    nodatahere.style.display="block";
+                    nodatahere.innerHTML="目前沒有任何待預訂的行程";
+                    footer.style.height="865px";
+                }
+                else{
+                    let pic=document.getElementById("picture");
+                    pic.src=bookData.data.attraction.image;
+                    let b_title=document.getElementById("b_title");
+                    b_title.innerHTML=bookData.data.attraction.name;
+                    let b_date=document.getElementById("b_date");
+                    b_date.innerHTML=bookData.data.date;
+                    let b_time=document.getElementById("b_time");
+                    b_time.innerHTML=bookData.data.time;
+                    let b_fee=document.getElementById("b_fee");
+                    let total_fee=document.getElementById("total_fee");
+                    b_fee.innerHTML=bookData.data.price;
+                    total_fee.innerHTML=bookData.data.price;
+                    let b_address=document.getElementById("b_address");
+                    b_address.innerHTML=bookData.data.attraction.address;
+                    let contactperson=document.getElementById("contactperson");
+                    contactperson.value=username;
+                    let contact_email=document.getElementById("contact_email");
+                    contact_email.value=useremail;
+                }
+            }
         }
     }
 }
@@ -82,11 +85,11 @@ function deletebooking(){
     delete_req.withCredentials = true;
     delete_req.send();
     delete_req.onload=function(){
-        user_name=document.getElementById("username");
-        user_name.innerHTML=username;
         let deleteData=JSON.parse(delete_req.responseText);
         if(deleteData!=null){
             window.location.href = "/booking";
+            user_name=document.getElementById("username");
+            user_name.innerHTML=username;
             first.innerHTML="";
             first_part.style.height="220px";
             line1.style.display="none";
